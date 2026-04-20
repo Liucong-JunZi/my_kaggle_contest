b6a8862ce0d5f959ea56ffb12cbe402a98bf3bcb
Liucong-JunZi <lc20040517@gmail.com>
2026-04-21 01:17:46 +0800

    Add EfficientNetV1 model configuration and metadata

---
+++ bird/effnet/effnetv1.py
@@ -0,0 +1,13 @@
+
+class config(): 
+    def __init__(self):
+        self.model_name = 'EfficientNetV1'
+        self.input_size = 224
+        self.num_classes = 1000
+        self.dropout_rate = 0.2
+        self.width_coefficient = 1.0
+        self.depth_coefficient = 1.0
+        self.resolution_coefficient = 1.0
+
+
+
